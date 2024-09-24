b7c15c73ad1520915de6c5b31d1c96ddaa750e30
Matthew Albrecht <matthewwilliamalbrecht@gmail.com>
2024-09-23 21:25:59 -0600

    hide onbaording flow if not owner

--- src/app/clubs/[clubId]/onboarding/questions/page.tsx
+++ src/app/clubs/[clubId]/onboarding/questions/page.tsx
@@ -1,15 +1,23 @@
 import { notFound } from "next/navigation"
 import { db } from "~/server/db"
 import { FormRecordClubQuestionSelection } from "./form-record-club-question-selection"
+import { auth } from "@clerk/nextjs/server"
 
 export default async function ClubsCreatePage({
 	params,
 }: {
 	params: { clubId: string }
 }) {
+	const user = auth()
 	const clubId = Number(params.clubId)
+
+	if (!user.userId) {
+		return notFound()
+	}
+
 	const club = await db.query.clubs.findFirst({
-		where: (club, { eq }) => eq(club.id, Number(clubId)),
+		where: (club, { eq, and }) =>
+			and(eq(club.id, Number(clubId)), eq(club.ownedById, user.userId)),
 	})
 
 	if (!club) {
--- src/app/clubs/[clubId]/onboarding/schedule/page.tsx
+++ src/app/clubs/[clubId]/onboarding/schedule/page.tsx
@@ -3,16 +3,23 @@ import { notFound } from "next/navigation"
 import { db } from "~/server/db"
 import { clubAlbums } from "~/server/db/schema"
 import { FormRecordClubCreateSchedule } from "./form-record-club-create-schedule"
+import { auth } from "@clerk/nextjs/server"
 
 export default async function ClubsOnboardingSchedulePage({
 	params,
 }: {
 	params: { clubId: string }
 }) {
+	const user = auth()
 	const clubId = Number(params.clubId)
 
+	if (!user.userId) {
+		return notFound()
+	}
+
 	const club = await db.query.clubs.findFirst({
-		where: (club, { eq }) => eq(club.id, Number(clubId)),
+		where: (club, { eq, and }) =>
+			and(eq(club.id, Number(clubId)), eq(club.ownedById, user.userId)),
 	})
 
 	if (!club) {
